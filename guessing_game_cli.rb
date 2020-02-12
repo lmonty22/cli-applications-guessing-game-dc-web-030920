dcdfe945fcd74f920cf6ef05e9f25890fecf0c0b
Lindsay Montgomery <lindsay.montgomery@olympian.org>
2020-02-12 20:41:27 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,26 @@
 # Code your solution here!
+def user_input 
+  gets.chomp
+end 
+  
+def random_number 
+  rand(1..6)
+end 
+ 
+def statements(num, input)
+      new_value = input.to_i
+          if new_value == num
+              puts "You guessed the correct number!"
+          else
+              puts "Sorry! The computer guessed #{num}."
+          end
+end 
+
+def run_guessing_game
+  input = user_input
+   if input == 'exit'
+         puts "Goodbye!"
+      else
+        statements(random_number, input)
+    end
+end 
